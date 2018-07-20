ff7549b63330c912219b0d28b24e2b4fe3b3ee76
Droffner, Andrew (ad718x) <ad718x@us.att.com>
2018-07-20 15:38:11 -0400

    [INC-000] Create an "emailBuildReport" DSL command to send a boilerplate when Jenkins is done.

---
+++ vars/emailBuildReport.groovy
@@ -0,0 +1,25 @@
+/* E-Mail Build Report.
+ *
+ * E-mail the build report to interested parties.
+ *
+ * Required Plugins: Git Plugin, ??
+ */
+
+def call(mailToList) {
+    node {
+        mail to: mailToList,
+        subject: "[CI Process][${currentBuild.currentResult}][Build ${env.BUILD_ID}][${env.JOB_NAME}] Jenkins Done",
+        body: """
+
+The CI Processing pipeline finished building JOB ${env.BUILD_ID} with ${currentBuild.currentResult} status.
+
+    See Jenkins URL: ${env.BUILD_URL}
+
+Job: ${env.JOB_NAME}
+Branch: ${env.BRANCH_NAME}
+Build Number: ${env.BUILD_ID}
+
+This is an automated e-mail. Please, do not reply to sender.
+"""
+    }
+}
