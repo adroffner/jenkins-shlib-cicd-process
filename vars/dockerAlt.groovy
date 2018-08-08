54749e0fee141b89a29962fc57b252f9306d26d8
Droffner, Andrew (ad718x) <ad718x@us.att.com>
2018-08-08 16:22:46 -0400

    [INC-2308]Create a "dockerAlt.*" to handle the same API as the non-working docker.* parts.

---
+++ vars/dockerAlt.groovy
@@ -0,0 +1,13 @@
+/* Docker Alternative.
+ *
+ * Alternative Docker DSL handles problems using the real one.
+ *
+ * Required Plugins: Docker Plugins
+ */
+
+def build(String imageName) {
+	// docker.build(..) errors (https://issues.jenkins-ci.org/browse/JENKINS-31507)
+	sh "docker build -t ${imageName} ./"
+	def newImage = docker.image(imageName)
+	return newImage
+}
