485575ac22c6411053f32b7a7555865ef4f10ad9
Droffner, Andrew (ad718x) <ad718x@us.att.com>
2018-08-08 17:23:01 -0400

    [INC-2308] Need "sudo docker" here; the othe script isn't in view.

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -16,6 +16,7 @@ def call(String imageName) {
 		def fullName = fullImageName(imageName)
 
 		// docker.build(..) errors (https://issues.jenkins-ci.org/browse/JENKINS-31507)
-		sh "docker build -t ${fullName} ./"
+		// docker command requires "sudo" on this server?
+		sh "sudo docker build -t ${fullName} ./"
 	}
 }
