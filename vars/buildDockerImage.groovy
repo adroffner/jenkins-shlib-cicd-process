f6a5f2784775f8a2932ab1191b2f1c0574d16a3b
Droffner, Andrew (ad718x) <ad718x@us.att.com>
2018-08-08 16:54:06 -0400

    [INC-2308] Created buildDockerImage "baseImageName" DSL that adds the tags for us.

---
+++ vars/buildDockerImage.groovy
@@ -0,0 +1,21 @@
+/* Build Docker Image.
+ *
+ * Build Docker image based on the current Git branch.
+ *
+ * Required Plugins: "Git Plugin"
+ */
+
+def fullImageName(String imageName) {
+	def dockerNamespace = incubateConfig.DOCKER_NAMESPACE
+	def imageNameGitTag = "${dockerNamespace}/${imageName}:${env.BUILD_ID}_${env.GIT_COMMIT}"
+	return imageNameGitTag
+}
+
+def call(String imageName) {
+	node {
+		def fullName = fullImageName(imageName)
+
+		// docker.build(..) errors (https://issues.jenkins-ci.org/browse/JENKINS-31507)
+		sh "docker build -t ${fullName} ./"
+	}
+}
