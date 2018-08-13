ee15c6fe4af3f0fb655f1edb3a9dcd503407bcc3
Droffner, Andrew (ad718x) <ad718x@us.att.com>
2018-08-13 12:55:11 -0400

    [INC-2308] Put as many facts as possible into incubateConfig.groovy for building.

--- vars/incubateConfig.groovy
+++ vars/incubateConfig.groovy
@@ -3,6 +3,9 @@
  * This file stores configuration variables for this shared library.
  **/
 
+HTTP_PROXY = 'http://one.proxy.att.com:8080'
+HTTPS_PROXY = 'http://one.proxy.att.com:8080'
+
 // Private Docker Registry for Incubate Team
 DOCKER_REGISTRY_URL = 'https://dockercentral.it.att.com:5100'
 DOCKER_NAMESPACE = 'dockercentral.it.att.com:5100/com.att.dev.argos'
@@ -10,3 +13,7 @@ DOCKER_NAMESPACE = 'dockercentral.it.att.com:5100/com.att.dev.argos'
 // NOTE: DOCKER_CRED_ID matches Jenkins Global credentials-ID.
 // See Docker Workflow Pipeline Plugin for help.
 DOCKER_CRED_ID = 'docker-credentials-id'
+
+// SSH Publish Plugin Target Directory.
+// docker-compose Directory on remote deployment
+DOCKER_COMPOSE_DIR = '/home/ad718x/test-docker-deploy'
