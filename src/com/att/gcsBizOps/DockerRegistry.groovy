e4b32f6c07a42563b4e6da0412bd927036712b0c
Droffner, Andrew (ad718x) <ad718x@us.att.com>
2018-08-14 20:06:17 -0400

    [INC-2308] What does it take to get Groovy to make a constant string public?

--- src/com/att/gcsBizOps/DockerRegistry.groovy
+++ src/com/att/gcsBizOps/DockerRegistry.groovy
@@ -6,37 +6,16 @@
 package com.att.gcsBizOps;
 
 // Private Docker Registry for Incubate Team
-def DOCKER_REGISTRY_URL = 'https://dockercentral.it.att.com:5100'
-def DOCKER_NAMESPACE = 'dockercentral.it.att.com:5100/com.att.dev.argos'
+static final String DOCKER_REGISTRY_URL = 'https://dockercentral.it.att.com:5100'
+static final String DOCKER_NAMESPACE = 'dockercentral.it.att.com:5100/com.att.dev.argos'
 
 // NOTE: DOCKER_CRED_ID matches Jenkins Global credentials-ID.
 // See Docker Workflow Pipeline Plugin for help.
-def DOCKER_CRED_ID = 'docker-credentials-id'
+static final String DOCKER_CRED_ID = 'docker-credentials-id'
 
 // SSH Publish Plugin Target Directory.
 // docker-compose Directory on remote deployment
 // TODO: Can we introspect this for the SSH Publish Plugin's config?
-def DOCKER_COMPOSE_DIR = '/home/ad718x/test-docker-deploy'
-
-/**
-class DockerRegistryConfig {
-
-	def getRegistryURL() {
-		return DOCKER_REGISTRY_URL
-	}
-
-	def getNamespace() {
-		return DOCKER_NAMESPACE
-	}
-
-	def getCredentialsId() {
-		return DOCKER_CRED_ID
-	}
-
-	def getDockerComposeDir() {
-		return DOCKER_COMPOSE_DIR
-	}
-}
-**/
+static final String DOCKER_COMPOSE_DIR = '/home/ad718x/test-docker-deploy'
 
 return this
