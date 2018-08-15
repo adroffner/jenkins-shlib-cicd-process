2b8921c5eebf61afb4a6d3ae617abc32ce372094
Droffner, Andrew (ad718x) <ad718x@us.att.com>
2018-08-15 10:47:44 -0400

    [INC-2308] Change cleanUpDocker DSL to run on every branch build for now.

--- vars/cleanUpDocker.groovy
+++ vars/cleanUpDocker.groovy
@@ -12,20 +12,23 @@ def call(String imageName, hostSSHCredentials) {
 	def fullImageName = buildDockerImage.fullImageName(imageName)
 
 	// SSH login and remove Docker objects.
+	// TODO: Should we run this this on every branch build, or is it too costly?
 	switch (env.BRANCH_NAME) {
-	'develop':
-	'master':
-		echo "Clean up Docker for ${fullImageName} ..."
-		sshagent(credentials: [hostSSHCredentials]) {
-			// Remove garbage containers.
-			sh 'docker rm $(docker ps -q -f status=created) || true'
-			sh 'docker rm $(docker ps -q -f  status=exited) || true'
-			sh 'docker rm $(docker ps -a -q -f status=dead) || true'
+		case 'develop':
+		case 'master':
 
-			// Remove dangling images and this temporay build image.
-			sh 'docker rmi $(docker images -q -f dangling=true)'
-			sh "docker rmi ${fullImageName}"
-		}
-		break
+		case env.BRANCH_NAME:
+			echo "Clean up Docker for ${fullImageName} ..."
+			sshagent(credentials: [hostSSHCredentials]) {
+				// Remove garbage containers.
+				sh 'docker rm $(docker ps -q -f status=created) || true'
+				sh 'docker rm $(docker ps -q -f  status=exited) || true'
+				sh 'docker rm $(docker ps -a -q -f status=dead) || true'
+
+				// Remove dangling images and this temporary build image.
+				sh 'docker rmi $(docker images -q -f dangling=true) || true'
+				sh "docker rmi ${fullImageName} || true"
+			}
+			break
 	} // end switch
 }
