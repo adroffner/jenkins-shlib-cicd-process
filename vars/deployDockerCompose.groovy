6feddaa8c89a00d566d46e7a84a35de0f217ad0c
Droffner, Andrew (ad718x) <ad718x@us.att.com>
2018-08-16 11:59:48 -0400

    [INC-2308] Create DSL steps for the last two code blocks; now we're close!

--- vars/deployDockerCompose.groovy
+++ vars/deployDockerCompose.groovy
@@ -3,48 +3,48 @@
  * Provide a DSL to deploy a Git project and start its Docker services.
  * There must be a docker-compose YAML file assigned to the tier, e.g. "dev".
  *
+ *    DSL: deployDockerCompose('dev', 'micro.dev', 'docker-credentials-id')
+ *
  * Required Plugins: "Git Plugin", "Publish Over SSH"
  */
 
-def call(String tier, String hostCredentials = null, deployDir='/tmp') {
-	node {
-		    // Set "develop" or "master".
-		    def tier = 'dev';
-		    if (env.BRANCH_NAME == 'master') {
-			tier = 'prod';
-		    }
+SERVER_TIER_LIST = ['dev', 'stage', 'prod']
 
-		    // Set the image TAG in docker-compose YAML.
-		    sh "perl -pi -e 's/:\\\$TAG\$/:${env.BUILD_ID}/;' ${env.PROJECT_SRC_PATH}/docker-compose-${tier}.yml"
+def call(String tier, String hostSSHCredentials, dockerCredentials) {
+	if (not SERVER_TIER_LIST.contains(tier)) {
+		error("Deployment Failed - Server Tier must go to a tier in: " + SERVER_TIER_LIST.join(', '))
+	}
 
-		    // Move the docker-compose YAML file over, download and run it.
-		    withCredentials([[$class: 'UsernamePasswordMultiBinding',
-		    	credentialsId: "${env.DOCKER_CRED_ID}",
-                    	usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS']]) {
+	// Set the image TAG in docker-compose YAML.
+	if (tier != 'prod') {
+		sh "perl -pi -e 's/:\\\$TAG\$/:${env.BUILD_ID}/;' ${env.PROJECT_SRC_PATH}/docker-compose-${tier}.yml"
+	}
 
-			// Use Publish Over SSH: https://jenkins.io/doc/pipeline/steps/publish-over-ssh/
-			sshPublisher(publishers: [
-				sshPublisherDesc(configName: 'micro.dev', // SSH Credentials
-				transfers: [
-					sshTransfer(
-						// excludes: '',
-						execCommand: """/bin/bash -c ' \\
+	// Move the docker-compose YAML file over, download and run it.
+	withCredentials([[$class: 'UsernamePasswordMultiBinding',
+		credentialsId: dockerCredentials,
+		usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS']]) {
+
+		// Use Publish Over SSH: https://jenkins.io/doc/pipeline/steps/publish-over-ssh/
+		sshPublisher(publishers: [
+			sshPublisherDesc(configName: hostSSHCredentials, // SSH Credentials
+			transfers: [
+				sshTransfer(
+				// excludes: '',
+				execCommand: """/bin/bash -c ' \\
 sudo docker login -u ${env.DOCKER_USER} -p ${env.DOCKER_PASS} -e nobody@att.com ${env.DOCKER_REGISTRY_URL} && \\
 sudo docker-compose -f ${env.DOCKER_COMPOSE_DIR}/${env.IMAGE_NAME}/docker-compose-${tier}.yml pull web && \\
 sudo docker-compose -f ${env.DOCKER_COMPOSE_DIR}/${env.IMAGE_NAME}/docker-compose-${tier}.yml down && \\
 sudo docker-compose -f ${env.DOCKER_COMPOSE_DIR}/${env.IMAGE_NAME}/docker-compose-${tier}.yml up -d'
-					""",
-					execTimeout: 120000, flatten: true,
-					// makeEmptyDirs: false, noDefaultExcludes: false,
-					// patternSeparator: '[, ]+',
-					remoteDirectory: "${env.IMAGE_NAME}",  // Relative PROJECT DIR
-					// remoteDirectorySDF: false, removePrefix: '',
-					sourceFiles: "docker-compose-${tier}.yml")],
-					// usePromotionTimestamp: false, useWorkspaceInPromotion: false,
-					verbose: true)],
-					failOnError: true)
-	  		} // end withCredentials
-
-	}
+""",
+				execTimeout: 120000, flatten: true,
+				// makeEmptyDirs: false, noDefaultExcludes: false,
+				// patternSeparator: '[, ]+',
+				remoteDirectory: "${env.IMAGE_NAME}",  // Relative PROJECT DIR
+				// remoteDirectorySDF: false, removePrefix: '',
+				sourceFiles: "docker-compose-${tier}.yml")],
+				// usePromotionTimestamp: false, useWorkspaceInPromotion: false,
+				verbose: true)],
+				failOnError: true)
+  		} // end withCredentials
 }
-
---
+++ vars/runUnitTestsDockerImage.groovy
@@ -0,0 +1,53 @@
+/* Run Unit Tests in Docker Image.
+ *
+ * Run the unit test suite in a Docker image.
+ *
+ * These test report artifacts will be published to Jenkins.
+ * The /home/bin/run_tests.sh scripts should generate them.
+ *
+ * Test Results:    unittest.xml
+ * Code Coverage:   coverage.xml
+ * Static Analysis: flake8.log
+ *
+ * DSL: runUnitTestsDockerImage("short_image_name")
+ *
+ * Required Plugins:
+ *	"Docker Plugins", "Git Plugin"
+ * 	"JUnit Plugin", "Cobertura Plugin", "Warnings Plugin"
+ */
+
+def call(String imageName) {
+	def fullImageName = buildDockerImage.fullImageName(imageName)
+	def unitTestImage = docker.image(fullImageName)
+
+	if (unitTestImage != null) {
+		// Start docker container and execute run_tests.sh
+		script {
+			sh """ mkdir ${env.WORKSPACE}/test-reports \\
+&& chmod 777 ${env.WORKSPACE}/test-reports \\
+&& docker run \\
+	--entrypoint="/home/bin/run_tests.sh" \\
+	--volume="${env.WORKSPACE}/test-reports:/tmp/" ${fullImageName}
+"""
+
+			// Publish unit test, coverage, and static analysis reports.
+			junit '**/unittest.xml'
+			cobertura(
+				autoUpdateHealth: false,
+				autoUpdateStability: false,
+				coberturaReportFile: '**/coverage.xml',
+				failUnhealthy: true,
+				failUnstable: true,
+				maxNumberOfBuilds: 0,
+				onlyStable: false, sourceEncoding: 'ASCII',
+				zoomCoverageChart: false)
+			warnings(
+				parserConfigurations: [[
+					parserName: 'Pep8',
+					pattern: '**/flake8.log'
+				]],
+				unstableTotalAll: '0',
+				usePreviousBuildAsReference: true)
+		}
+	}
+}
