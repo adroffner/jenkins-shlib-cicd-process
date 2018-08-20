79b825c7db2699759ee5d69f8cf5894d9c38591b
Droffner, Andrew (ad718x) <ad718x@us.att.com>
2018-08-20 12:48:47 -0400

    [INC-2308] Sometimes Groovy is as much a pita as Java!

--- src/com/att/gcsBizOps/DockerRegistry.groovy
+++ src/com/att/gcsBizOps/DockerRegistry.groovy
@@ -6,16 +6,16 @@
 package com.att.gcsBizOps;
 
 // Private Docker Registry for Incubate Team
-static final String DOCKER_REGISTRY_URL = 'https://dockercentral.it.att.com:5100'
-static final String DOCKER_NAMESPACE = 'dockercentral.it.att.com:5100/com.att.dev.argos'
+public static final String DOCKER_REGISTRY_URL = 'https://dockercentral.it.att.com:5100'
+public static final String DOCKER_NAMESPACE = 'dockercentral.it.att.com:5100/com.att.dev.argos'
 
 // NOTE: DOCKER_CRED_ID matches Jenkins Global credentials-ID.
 // See Docker Workflow Pipeline Plugin for help.
-static final String DOCKER_CRED_ID = 'docker-credentials-id'
+public static final String DOCKER_CRED_ID = 'docker-credentials-id'
 
 // SSH Publish Plugin Target Directory.
 // docker-compose Directory on remote deployment
 // TODO: Can we introspect this for the SSH Publish Plugin's config?
-static final String DOCKER_COMPOSE_DIR = '/home/ad718x/test-docker-deploy'
+public static final String DOCKER_COMPOSE_DIR = '/home/ad718x/test-docker-deploy'
 
 return this
