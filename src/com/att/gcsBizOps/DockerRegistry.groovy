7f251ce8d463a5b60a7d769e08cfafe752bbf425
Droffner, Andrew (ad718x) <ad718x@us.att.com>
2018-08-20 12:56:11 -0400

    [INC-2308] Work on reading class constants from Groovy.

--- src/com/att/gcsBizOps/DockerRegistry.groovy
+++ src/com/att/gcsBizOps/DockerRegistry.groovy
@@ -5,17 +5,17 @@
 
 package com.att.gcsBizOps;
 
-// Private Docker Registry for Incubate Team
-public static final String DOCKER_REGISTRY_URL = 'https://dockercentral.it.att.com:5100'
-public static final String DOCKER_NAMESPACE = 'dockercentral.it.att.com:5100/com.att.dev.argos'
+class Configs {
+	// Private Docker Registry for Incubate Team
+	public static final String DOCKER_REGISTRY_URL = 'https://dockercentral.it.att.com:5100'
+	public static final String DOCKER_NAMESPACE = 'dockercentral.it.att.com:5100/com.att.dev.argos'
 
-// NOTE: DOCKER_CRED_ID matches Jenkins Global credentials-ID.
-// See Docker Workflow Pipeline Plugin for help.
-public static final String DOCKER_CRED_ID = 'docker-credentials-id'
+	// NOTE: DOCKER_CRED_ID matches Jenkins Global credentials-ID.
+	// See Docker Workflow Pipeline Plugin for help.
+	public static final String DOCKER_CRED_ID = 'docker-credentials-id'
 
-// SSH Publish Plugin Target Directory.
-// docker-compose Directory on remote deployment
-// TODO: Can we introspect this for the SSH Publish Plugin's config?
-public static final String DOCKER_COMPOSE_DIR = '/home/ad718x/test-docker-deploy'
-
-return this
+	// SSH Publish Plugin Target Directory.
+	// docker-compose Directory on remote deployment
+	// TODO: Can we introspect this for the SSH Publish Plugin's config?
+	public static final String DOCKER_COMPOSE_DIR = '/home/ad718x/test-docker-deploy'
+}
