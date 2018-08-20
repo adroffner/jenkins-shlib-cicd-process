32dbc2a21fc537dad8abc8c502bbc70cfdd85bfc
Droffner, Andrew (ad718x) <ad718x@us.att.com>
2018-08-20 11:01:18 -0400

    [INC-2308] Finally, finish vars/deployDockerCompose.groovy with sensible parameters.

--- vars/deployDockerCompose.groovy
+++ vars/deployDockerCompose.groovy
@@ -3,21 +3,27 @@
  * Provide a DSL to deploy a Git project and start its Docker services.
  * There must be a docker-compose YAML file assigned to the tier, e.g. "dev".
  *
- *    DSL: deployDockerCompose('dev', 'micro.dev', 'docker-credentials-id')
+ *    DSL: deployDockerCompose(
+ *		'short_image_name', '/ssh/publisher/remote/dir',
+ *		'dev', 'micro.dev')
  *
  * Required Plugins: "Git Plugin", "Publish Over SSH"
  */
 
 SERVER_TIER_LIST = ['dev', 'stage', 'prod']
 
-def call(String tier, String hostSSHCredentials, dockerCredentials) {
+def call(String imageName, String remoteDirectory,
+		String tier, String hostSSHCredentials,
+		String dockerCredentials = 'docker-credentials-id',
+		String yamlFileDirectory = '.') {
 	if (not SERVER_TIER_LIST.contains(tier)) {
-		error("Deployment Failed - Server Tier must go to a tier in: " + SERVER_TIER_LIST.join(', '))
+		error("DEPLOYMENT FAILED: Server Tier must go to a tier in: "
+			+ SERVER_TIER_LIST.join(', '))
 	}
 
 	// Set the image TAG in docker-compose YAML.
 	if (tier != 'prod') {
-		sh "perl -pi -e 's/:\\\$TAG\$/:${env.BUILD_ID}/;' ${env.PROJECT_SRC_PATH}/docker-compose-${tier}.yml"
+		sh "perl -pi -e 's/:\\\$TAG\$/:${env.BUILD_ID}/;' ${yamlFileDirectory}/docker-compose-${tier}.yml"
 	}
 
 	// Move the docker-compose YAML file over, download and run it.
@@ -33,18 +39,18 @@ def call(String tier, String hostSSHCredentials, dockerCredentials) {
 				// excludes: '',
 				execCommand: """/bin/bash -c ' \\
 sudo docker login -u ${env.DOCKER_USER} -p ${env.DOCKER_PASS} -e nobody@att.com ${env.DOCKER_REGISTRY_URL} && \\
-sudo docker-compose -f ${env.DOCKER_COMPOSE_DIR}/${env.IMAGE_NAME}/docker-compose-${tier}.yml pull web && \\
-sudo docker-compose -f ${env.DOCKER_COMPOSE_DIR}/${env.IMAGE_NAME}/docker-compose-${tier}.yml down && \\
-sudo docker-compose -f ${env.DOCKER_COMPOSE_DIR}/${env.IMAGE_NAME}/docker-compose-${tier}.yml up -d'
+sudo docker-compose -f ${remoteDirectory}/${imageName}/docker-compose-${tier}.yml pull web && \\
+sudo docker-compose -f ${remoteDirectory}/${imageName}/docker-compose-${tier}.yml down && \\
+sudo docker-compose -f ${remoteDirectory}/${imageName}/docker-compose-${tier}.yml up -d'
 """,
 				execTimeout: 120000, flatten: true,
 				// makeEmptyDirs: false, noDefaultExcludes: false,
 				// patternSeparator: '[, ]+',
-				remoteDirectory: "${env.IMAGE_NAME}",  // Relative PROJECT DIR
+				remoteDirectory: "${imageName}",  // Relative PROJECT DIR
 				// remoteDirectorySDF: false, removePrefix: '',
 				sourceFiles: "docker-compose-${tier}.yml")],
 				// usePromotionTimestamp: false, useWorkspaceInPromotion: false,
 				verbose: true)],
 				failOnError: true)
-  		} // end withCredentials
+	} // end withCredentials
 }
