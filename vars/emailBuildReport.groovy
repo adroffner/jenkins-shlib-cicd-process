c9a59310fb9e2441f3954fccd43af1afaf9efa31
Droffner, Andrew (ad718x) <ad718x@us.att.com>
2018-09-08 03:08:15 -0400

    [MIC-1055] Catch e-mail ConnectException and print a message; this prevent a FAILURE build when e-mai lservers are down.

--- vars/emailBuildReport.groovy
+++ vars/emailBuildReport.groovy
@@ -9,9 +9,10 @@
 
 def call(mailToList) {
     node {
-        mail to: mailToList.join(','),
-        subject: "[CI Process][${currentBuild.currentResult}][Build ${env.BUILD_ID}][${env.JOB_NAME}] Jenkins Done",
-        body: """
+	try {
+	    mail to: mailToList.join(','),
+	    subject: "[CI Process][${currentBuild.currentResult}][Build ${env.BUILD_ID}][${env.JOB_NAME}] Jenkins Done",
+	    body: """
 
 The CI Processing pipeline finished building JOB ${env.BUILD_ID} with ${currentBuild.currentResult} status.
 
@@ -23,5 +24,8 @@ Build Number: ${env.BUILD_ID}
 
 This is an automated e-mail. Please, do not reply to sender.
 """
+	catch(ConnectException exception) {
+	    echo("E-mail Report Lost: ${exception}")
+	}
     }
 }
