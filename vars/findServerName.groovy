236071fab7fca114cc3b1b8e7577dce4d19b11a5
Finarovsky, Julia (jf147y) <jf147y@us.att.com>
2018-11-08 11:19:02 -0500

    [MIC-1187] Changed to File.separator..................................

--- vars/findServerName.groovy
+++ vars/findServerName.groovy
@@ -10,15 +10,17 @@
     staticMethod org.codehaus.groovy.runtime.DefaultGroovyMethods getText java.io.File
     new java.io.File java.lang.String
     java.lang.String
+    staticField java.io.File separator
 */
+
 
 def call(serverConfig="server_config.py") {
   try {
-      server_config = findFiles(glob: "**/${serverConfig}")
+      server_config = findFiles(glob: "**${File.separator}${serverConfig}")
       server_config_path = "${server_config[0].path}"
       def projectName = new File(server_config_path).parent
-      baseDir = "${env.WORKSPACE}/$projectName"
-      server_config_contents = new File("${baseDir}/${serverConfig.py}").text
+      baseDir = "${env.WORKSPACE}${File.separator}${projectName}"
+      server_config_contents = new File("${baseDir}${File.separator}${serverConfig}").text
       def serverName = "${server_config_contents}"=~['"].*\.web\..*:\d+['"]/
       serverName = serverName[0].replaceAll("\"", "").replaceAll("'", "")
       println ("Server name found in server_config.py: ${serverName}")
