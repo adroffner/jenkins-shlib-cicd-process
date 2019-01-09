69944e580ca8df4a335325eeda07af1d8d4ef6a8
Finarovsky, Julia (jf147y) <jf147y@us.att.com>
2019-01-09 08:35:56 -0500

    [INC-2328] Added print statement.................................

--- vars/findServerName.groovy
+++ vars/findServerName.groovy
@@ -22,6 +22,7 @@ def call(serverConfig="server_config.py") {
       def projectName = new File(server_config_path).parent
       baseDir = "${env.WORKSPACE}${File.separator}${projectName}"
       filePath = "${baseDir}${File.separator}${serverConfig}"
+      println("printing file path")
       println(filePath)
       
 
