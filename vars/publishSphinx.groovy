f90200e8ac696b2d34397664b27f5df7d55dca09
Finarovsky, Julia (jf147y) <jf147y@us.att.com>
2019-01-11 12:41:34 -0500

    [INC-2328] Changed publishsphinx permissions again...........................................

--- vars/publishSphinx.groovy
+++ vars/publishSphinx.groovy
@@ -3,7 +3,7 @@ def call(String imageName) {
   script {
     	sh """  mkdir ${env.WORKSPACE}/docs \\
 && chmod 777 ${env.WORKSPACE}/docs \\
-&& docker run --rm \\
+&& sudo docker run --rm \\
 	--user="www-data" \\
 	--entrypoint="/home/bin/build_sphinx.sh" \\
 	--volume="${env.WORKSPACE}/docs:/home/bin/app/greta_api/documentation/_build/html" ${fullImageName}
