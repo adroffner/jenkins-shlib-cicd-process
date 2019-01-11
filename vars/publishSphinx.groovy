0c9b20cc605d92104e6c79b03e61e9f8bf968826
Finarovsky, Julia (jf147y) <jf147y@us.att.com>
2019-01-11 15:17:13 -0500

    [INC-2328] Added directory checking for docs folder.........................................

--- vars/publishSphinx.groovy
+++ vars/publishSphinx.groovy
@@ -1,16 +1,22 @@
 def call(String imageName) {
   def fullImageName = buildDockerImage.fullImageName(imageName)
   script {
-    	sh """mkdir ${env.WORKSPACE}/documentation \\
-&& chmod 777 ${env.WORKSPACE}/documentation \\
-&& sudo docker run --rm \\
-	--user="www-data" \\
-	--entrypoint="/home/bin/build_sphinx.sh" \\
-	--volume="${env.WORKSPACE}/documentation:/home/app/greta_api/documentation/" ${fullImageName} \\
- && echo "After sudo docker run" \\
- && pwd \\
- && ls \\
- && ls ${env.WORKSPACE}/documentation
-"""
+      sh """
+        if [ ! -d "${env.WORKSPACE}/documentation"]; then
+          mkdir ${env.WORKSPACE}/documentation
+        fi
+      """
+
+    	sh """ 
+        chmod 777 ${env.WORKSPACE}/documentation \\
+        && sudo docker run --rm \\
+        --user="www-data" \\
+        --entrypoint="/home/bin/build_sphinx.sh" \\
+        --volume="${env.WORKSPACE}/documentation:/home/app/greta_api/documentation/" ${fullImageName} \\
+        && echo "After sudo docker run" \\
+        && pwd \\
+        && ls \\
+        && ls ${env.WORKSPACE}/documentation
+      """
   }
 }
