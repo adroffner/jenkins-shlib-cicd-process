6538000dd1556270aff210a9f35c609d809e48b7
Finarovsky, Julia (jf147y) <jf147y@us.att.com>
2019-01-11 15:05:32 -0500

    [INC-2328] Changed folder paths.........................................

--- vars/publishSphinx.groovy
+++ vars/publishSphinx.groovy
@@ -1,17 +1,17 @@
 def call(String imageName) {
   def fullImageName = buildDockerImage.fullImageName(imageName)
   script {
-    	sh """rmdir ${env.WORKSPACE}/docs \\
-        && mkdir ${env.WORKSPACE}/docs \\
-&& chmod 777 ${env.WORKSPACE}/docs \\
+    	sh """rmdir ${env.WORKSPACE}/documentation \\
+        && mkdir ${env.WORKSPACE}/documentation \\
+&& chmod 777 ${env.WORKSPACE}/documentation \\
 && sudo docker run --rm \\
 	--user="www-data" \\
 	--entrypoint="/home/bin/build_sphinx.sh" \\
-	--volume="${env.WORKSPACE}/docs:/home/app/greta_api/documentation/_build/html" ${fullImageName}
+	--volume="${env.WORKSPACE}/documentation:/home/app/greta_api/documentation/" ${fullImageName} \\
  && echo "After sudo docker run" \\
  && pwd \\
  && ls \\
- && ls ${env.WORKSPACE}/docs
+ && ls ${env.WORKSPACE}/documentation
 """
   }
 }
